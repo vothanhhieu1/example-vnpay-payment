a5a3408c4e883a33ba8acc0d7345179c46108a18
next_hieu.vo <hieu.vo@nexpando.com>
2022-05-24 15:39:22 +0700

    demo multi port

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, PaymentDemoApplication} from './application';
+import {ApplicationConfig, PaymentDemoApplication, PingApplication} from './application';
 
 export * from './application';
 
@@ -33,6 +33,42 @@ if (require.main === module) {
     },
   };
   main(config).catch(err => {
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}
+
+export async function runPing(options: ApplicationConfig = {}) {
+  const app = new PingApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
+
+if (process.env.PING) {
+  // Run the application
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? 3003),
+      host: process.env.HOST,
+      // The `gracePeriodForClose` provides a graceful close for http/https
+      // servers with keep-alive clients. The default value is `Infinity`
+      // (don't force-close). If you want to immediately destroy all sockets
+      // upon stop, set its value to `0`.
+      // See https://www.npmjs.com/package/stoppable
+      gracePeriodForClose: 5000, // 5 seconds
+      openApiSpec: {
+        // useful when used with OpenAPI-to-GraphQL to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  runPing(config).catch(err => {
     console.error('Cannot start the application.', err);
     process.exit(1);
   });
